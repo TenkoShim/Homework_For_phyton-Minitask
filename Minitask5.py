7822b0cad78f9d888d157c9990ecd7b61ba9bb86
TenkoShim <99762128+TenkoShim@users.noreply.github.com>
2023-10-16 23:16:19 +0700

    Add files via upload

---
+++ Minitask5.py
@@ -0,0 +1,17 @@
+def specialize(fun, *args, **kwargs):
+    def in_specialize(*new_args, **new_kwargs):
+        all_args = args + new_args
+        all_kwargs = {**kwargs, **new_kwargs}
+        return fun(*all_args, **all_kwargs)
+
+    return in_specialize
+
+
+def sum(x, y):
+    return x + y
+
+
+plus_one = specialize(sum, y=1)
+just_two = specialize(sum, 1, 1)
+print(plus_one(10))
+print(just_two())
