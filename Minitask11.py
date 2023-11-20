1ef5eb9cb802cb97289d1ca1c43b6b9804a67a05
TenkoShim <99762128+TenkoShim@users.noreply.github.com>
2023-11-20 22:57:09 +0700

    Add files via upload

---
+++ Minitask11.py
@@ -0,0 +1,14 @@
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class SingletonClass(metaclass=SingletonMeta):
+    pass
+
+obj1 = SingletonClass()
+obj2 = SingletonClass()
+print(id(obj1) == id(obj2))
