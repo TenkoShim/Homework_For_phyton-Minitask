bc3db333e8f1d2c91856a4f233b8258d61a27413
Tenko <atom612@mail.ru>
2023-11-27 23:05:28 +0700

    changes for review

--- Minitask2.py
+++ Minitask2.py
@@ -1,14 +1,11 @@
-def min_len(len_1,len_2):
-    if(len_1<len_2):
-        return len_1
-    else:
-        return len_2
-def cortege(a,b,lenght):
-    result=[]
-    for i in range(lenght):
-        result.append([a[i], b[i]])
+def zip(a, b, length):
+    result = []
+    for i in range(length):
+        result.append((a[i], b[i]))
     return result
-a=list(map(int,input("Enter Elements in int :").split()))
-b=list(map(str,input("Enter Elements in str :").split()))
-lenght=min_len(len(a),len(b))
-print(cortege(a,b,lenght))
+
+
+a = list(map(int, input("Enter Elements in int :").split()))
+b = list(map(str, input("Enter Elements in str :").split()))
+length = min(len(a), len(b))
+print(zip(a, b, length))
