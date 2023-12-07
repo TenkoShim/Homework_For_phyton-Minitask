1a5e35b1ff64be55d6c401d8b449ddf0adeb7f26
Tenko <atom612@mail.ru>
2023-12-07 22:14:59 +0700

    responded to comments

--- Minitask4.py
+++ Minitask4.py
@@ -1,11 +1,22 @@
-def cortege(d):
-    new_dict={}
+def reversed_dictionary(d):
+    new_dict = {}
     for i in d.keys():
-        if(d[i] in new_dict):
-            s=(new_dict[d[i]],i)
-            new_dict[d[i]]=s
-            continue
-        new_dict[d[i]] = i
+        key = d[i]
+        try:
+            if key in new_dict:
+                if type(new_dict[key]) == tuple:
+                    s = (i,) + new_dict[key]
+                else:
+                    s = (i, new_dict[key])
+                new_dict[d[i]] = s
+                continue
+            new_dict[key] = i
+        except TypeError:
+            print(f"{key} is not hashable")
+
     return new_dict
-d={"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}
-print(cortege(d))
+
+
+# d={"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}
+d = {"Ivanov": 97832, "Ivanov2": 97832, "Petrov": 55521, "Kuznecov": 97832}
+print(reversed_dictionary(d))
