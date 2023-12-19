f78ab64e781026e0230040a23309feee0877ce93
Tenko <atom612@mail.ru>
2023-12-20 04:10:46 +0700

    More safely

--- Minitask10.py
+++ Minitask10.py
@@ -1,29 +1,33 @@
 from collections import OrderedDict
 
+
 class LRUCache:
     def __init__(self, capacity=16):
-        self.capacity = capacity
+        self.__capacity = capacity
         self.cache = OrderedDict()
 
     def put(self, key, value):
         if key in self.cache:
             del self.cache[key]
-        elif len(self.cache) >= self.capacity:
+        elif len(self.cache) >= self.__capacity:
             self.cache.popitem(last=False)
-
         self.cache[key] = value
 
     def get(self, key):
         if key not in self.cache:
             return None
-        value = self.cache[key]
-        del self.cache[key]
-        self.cache[key] = value
-        return value
-c=LRUCache()
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
-for i in range(18):
-    c.put(i, i**2)
-value=c.get("A")
+    @property
+    def capacity(self):
+        return self.__capacity
+
+
+c = LRUCache(capacity=20)
+print(c.capacity)
+for i in range(c.capacity):
+    c.put(i, i ** 2)
+value = c.get("A")
 print(value)
 print(vars(c))
